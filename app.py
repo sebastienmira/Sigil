bb6e912c8a7243d7024a2a5f054e54959df7290c
sebastienmira <sebastienm.2001@gmail.com>
2024-05-22 00:33:28 +0300

    html fixed

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from flask_session import Session
 
 app = Flask(__name__)
 
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
