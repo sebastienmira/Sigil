e0ced69bd97fef6ecd59f85de47304ee5242ad51
sebastienmira <sebastienm.2001@gmail.com>
2024-05-22 12:44:43 +0300

    Error handling and route change

--- app.py
+++ app.py
@@ -9,12 +9,16 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-@app.route('/', methods=["GET","POST"])
-def index():
+@app.route('/encrypt', methods=["GET","POST"])
+def encrypt():
     if request.method =="POST":
         plain=request.form.get("plain")
         key=request.form.get("key")
-        encrypted=crypto.substitution(plain,key)
-        return render_template('index.html', encrypted=encrypted, plain=plain, key=key)
-
-    return render_template('index.html')
+        
+        try:
+            encrypted=crypto.substitution(plain,key)
+            return render_template('encrypt.html', encrypted=encrypted, plain=plain, key=key)
+        except ValueError as err:
+            error=str(err)
+            return render_template('encrypt.html', error=error, plain=plain, key=key)
+    return render_template("encrypt.html")
