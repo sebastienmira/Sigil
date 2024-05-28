0b1c9fcd131641d3718c2840fd0f3ea60fdeaa06
sebastienmira <sebastienm.2001@gmail.com>
2024-05-29 00:29:44 +0300

    experiences with permutations

--- auxiliary.py
+++ auxiliary.py
@@ -12,3 +12,27 @@ for i in freqList:
     freqInDict.append(i[0])
 
 print(freqList)
+
+
+
+
+
+'''
+permutations=list(itertools.product(*lista))
+print(permutations)
+result=[]
+for i in range(len(permutations[0])): #lenkey
+    prelresult=[]
+    for j in permutations: 
+        ctr=0
+        for k in j:
+            if k>i:
+                ctr=ctr+1
+        if ctr==0:
+            prelresult.append(j)
+    for k in prelresult:
+        permutations.pop(permutations.index(k))
+        result.append(k)
+print(permutations)
+print(result)
+'''
